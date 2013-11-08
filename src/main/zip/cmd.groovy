6fe4981874dd1327d67586b27c2a6852fb3de9f4
drh <drh@us.ibm.com>
2013-11-08 12:02:38 -0500

    editited plugin build.xml to not hard code old anthill server
    Change-Id: I639fce2dc926215fa5df8f06ef70b5dc5ddfbfbb

--- src/main/zip/cmd.groovy
+++ src/main/zip/cmd.groovy
@@ -1,12 +1,28 @@
-//This is an example step groovy to show the proper use of APTool
+/* This is an example step groovy to show the proper use of APTool
+ * In order to use import these utilities, you have to use the "pluginutilscripts" jar
+ * that comes bundled with this plugin example. 
+ */
 import com.urbancode.air.AirPluginTool;
 import com.urbancode.air.CommandHelper;
 
-def apTool = new AirPluginTool(this.args[0], this.args[1]) //assuming that args[0] is input props file and args[1] is output props file
+/* This gets us the plugin tool helper. 
+ * This assumes that args[0] is input props file and args[1] is output props file.
+ * By default, this is true. If your plugin.xml looks like the example. 
+ * Any arguments you wish to pass from the plugin.xml to this script that you don't want to 
+ * pass through the step properties can be accessed using this argument syntax
+ */
+def apTool = new AirPluginTool(this.args[0], this.args[1]) 
 
+/* Here we call getStepProperties() to get a Properties object that contains the step properties
+ * provided by the user. 
+ */
 def props = apTool.getStepProperties();
 
-def propValue = props['propName'];
+/* This is how you retrieve properties from the object. You provide the "name" attribute of the 
+ * <property> element 
+ * 
+ */
+def dirOffset = props['dirOffset'];
 
 //example commandHelper
 def workDir = new File(".");
@@ -14,7 +30,7 @@ def ch = new CommandHelper(workDir);
 def args = ['fileToExecute', 'arg1', 'arg2'];
 ch.runCommand("Message to display about running command", args);
 
-//do stuff here
+//Set an output property
 apTool.setOutputProperty("outPropName", "outPropValue");
 
 apTool.storeOutputProperties();//write the output properties to the file
