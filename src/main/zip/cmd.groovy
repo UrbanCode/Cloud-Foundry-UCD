2fccd3ec146bdb920f014ce0cdd9ddd22e8199c2
ajrosape@us.ibm.com <ajrosape@us.ibm.com>
2014-03-04 16:40:40 -0500

    Adding copyrights to template
    Change-Id: I0a6d66e6b2c68b69cbee485f8f206865379039bc

--- src/main/zip/cmd.groovy
+++ src/main/zip/cmd.groovy
@@ -1,3 +1,11 @@
+/*
+* Licensed Materials - Property of IBM Corp.
+* IBM UrbanCode Deploy
+* (c) Copyright IBM Corporation 2011, 2014. All Rights Reserved.
+*
+* U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by
+* GSA ADP Schedule Contract with IBM Corp.
+*/
 /* This is an example step groovy to show the proper use of APTool
  * In order to use import these utilities, you have to use the "pluginutilscripts" jar
  * that comes bundled with this plugin example. 
