5f8cb846d0d2e16edd1e83821b481d54120477c3
bstump <bstump@us.ibm.com>
2015-07-07 14:24:07 -0400

    139172 Update formatting, removing tabs and whitespace
    Change-Id: I9a4b43052d287932e39b735e624643ee4a21533c

--- src/main/zip/cmd.groovy
+++ src/main/zip/cmd.groovy
@@ -10,8 +10,8 @@
  * In order to use import these utilities, you have to use the "pluginutilscripts" jar
  * that comes bundled with this plugin example. 
  */
-import com.urbancode.air.AirPluginTool;
-import com.urbancode.air.CommandHelper;
+import com.urbancode.air.AirPluginTool
+import com.urbancode.air.CommandHelper
 
 /* This gets us the plugin tool helper. 
  * This assumes that args[0] is input props file and args[1] is output props file.
@@ -24,21 +24,22 @@ def apTool = new AirPluginTool(this.args[0], this.args[1])
 /* Here we call getStepProperties() to get a Properties object that contains the step properties
  * provided by the user. 
  */
-def props = apTool.getStepProperties();
+def props = apTool.getStepProperties()
 
 /* This is how you retrieve properties from the object. You provide the "name" attribute of the 
  * <property> element 
- * 
  */
-def dirOffset = props['dirOffset'];
+def dirOffset = props['dirOffset']
 
 //example commandHelper
-def workDir = new File(".");
-def ch = new CommandHelper(workDir);
-def args = ['fileToExecute', 'arg1', 'arg2'];
-ch.runCommand("Message to display about running command", args);
+def workDir = new File(".")
+def ch = new CommandHelper(workDir)
+def args = ['fileToExecute', 'arg1', 'arg2']
+ch.runCommand("Message to display about running command", args)
 
 //Set an output property
-apTool.setOutputProperty("outPropName", "outPropValue");
+apTool.setOutputProperty("outPropName", "outPropValue")
 
-apTool.storeOutputProperties();//write the output properties to the file
+//write the output properties to the file
+apTool.storeOutputProperties()
+
